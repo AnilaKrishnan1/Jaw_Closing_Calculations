6e03c878b16e69f577b1d1b1bed00f9c1d0c9627
AnilaKrishnan1 <anilak9405@gmail.com>
2026-08-09 04:07:41 -0500

    Angular velocity+Angular displacement graphs

---
+++ Displacement_Graphs.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("Tyrannosaurus_jaw_closing_accelerations.xlsx - Sheet1.csv", header = None) 
+time = data.iloc[1:33, 31]
+time = pd.to_numeric(time, errors="coerce") # convert to numbers
+valid_time = time.notna()
+time = time[valid_time]
+
+for i in range(4):
+     alpha = float(data.iloc[2+i, 27]) #angular acceleration
+     angular_displacement = 0.5 * alpha * time**2
+     plt.plot(time,angular_displacement)
+     plt.xlabel("Time (s)")
+     plt.ylabel("Angular displacement (radians)")
+     plt.title(f"T. rex Jaw Closing: Angular displacement vs. Time for angular acceleration\n a = {alpha} rad/ s^2")
+     plt.grid(True)
+     plt.show()
